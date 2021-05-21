2b4685223543ed426f22567979d1ca9d0bbd6b09
IlseMS90 <74887254+IlseMS90@users.noreply.github.com>
2021-05-21 01:35:52 -0500

    headerandfooter1

--- total_look/src/components/navbar/Navbar.js
+++ total_look/src/components/navbar/Navbar.js
@@ -4,16 +4,12 @@ import Logo from '../../assets/logo.png'
 import cart from '../../assets/cart.png'
 import eyeglass from '../../assets/eyeglass.png'
 import moreoffers from '../../assets/Group 55.png'
-import { SelectClothes } from '../../components/selects/selectClothes'
-import { SelectShoes } from '../../components/selects/SelectShoes'
-import { SelectSports } from '../../components/selects/SelectSports'
-import { SelectAccesories } from '../../components/selects/SelectAccesories'
-import { SelectKids } from '../../components/selects/SelectKids'
 
 
-export const Navbar = () => {
+export default function Navbar() {
     return (
         <div>
+                    <div>
             <div className="navbar">
 
                 <img className="logotype" src={Logo} alt="Logo totallook" />
@@ -33,6 +29,8 @@ export const Navbar = () => {
                 <h1>MORE OFFERS</h1>
                 <img className="moreoffers" src={moreoffers} alt="more offers" />
             </div>
+        </div>
+            
         </div>
     )
 }
